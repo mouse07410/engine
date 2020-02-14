e005f916af3364aacf2dab088287c088f1601f46
Mouse <mouse008@gmail.com>
2020-02-14 10:04:31 -0500

    Build for OpenSSL-3 first, then for 1.1.1

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
-sudo date && OPENSSL_DIR=/opt/local ./build-3.sh && sudo cp build/bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp build/bin/gost*sum /opt/local/bin/ 
+sudo date 
 
-OPENSSL_DIR=$HOME/openssl-3 ./build-3.sh && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
+OPENSSL_DIR=$HOME/openssl-3 ./build-3.sh 2>&1 | tee ossl3-build.txt && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
+
+OPENSSL_DIR=/opt/local ./build-3.sh 2>&1 | tee ossl111-build.txt && sudo cp build/bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp build/bin/gost*sum /opt/local/bin/ 

8ee44ff1bf27aad05c0ebd0298ddc470ed08eb08
Mouse <mouse008@gmail.com>
2020-05-04 09:22:33 -0400

    Some fixes

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,6 @@
 
 sudo date 
 
-OPENSSL_DIR=$HOME/openssl-3 ./build-3.sh 2>&1 | tee ossl3-build.txt && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
+OPENSSL_DIR="" ./build-3.sh 2>&1 | tee ossl3-build.txt && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
 
 OPENSSL_DIR=/opt/local ./build-3.sh 2>&1 | tee ossl111-build.txt && sudo cp build/bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp build/bin/gost*sum /opt/local/bin/ 

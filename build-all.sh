fb1d7dfa8b49956f2d823ae5f74a037b367d9e44
Mouse <mouse008@gmail.com>
2020-05-04 11:26:33 -0400

    Make build-all.sh build from two branches for OpenSSL 3.0 and 1.1.1

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,10 @@
 
 sudo date 
 
+git checkout master
+
 OPENSSL_DIR="" ./build-3.sh 2>&1 | tee ossl3-build.txt && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
+
+git checkout openssl_1.1.0
 
 OPENSSL_DIR=/opt/local ./build-3.sh 2>&1 | tee ossl111-build.txt && sudo cp build/bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp build/bin/gost*sum /opt/local/bin/ 

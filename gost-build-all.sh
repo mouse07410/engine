4ced668f4da90a3f0d870c8ab7ea2d10886e2ade
Mouse <mouse008@gmail.com>
2020-05-04 17:28:53 -0400

    Ensure you finish with master being the current branch

--- gost-build-all.sh
+++ gost-build-all.sh
@@ -15,3 +15,5 @@ if [ "$1" = "pull" ]; then
 fi
 
 OPENSSL_DIR=/opt/local ./gost-build-3.sh 2>&1 | tee ossl111-build.txt && sudo cp bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp bin/gost*sum /opt/local/bin/ 
+
+git checkout master

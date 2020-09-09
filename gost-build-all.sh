537b0cc26b0b1306ce88ab6e0720ef7f44d14ad0
Mouse <mouse008@gmail.com>
2020-09-09 09:29:47 -0400

    Add auto-builders to openssl_1_1_1 branch

---
+++ gost-build-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+sudo date 
+
+rm -rf build
+rm -f bin/gost.1.1.dylib
+
+git checkout master
+if [ "$1" = "pull" ]; then
+  git pull
+fi
+
+OPENSSL_DIR="" ./gost-build-3.sh 2>&1 | tee ossl3-build.txt && cp build/bin/gost.3.0.dylib ~/openssl-3/lib/engines-3/ && cp build/bin/gost*sum ~/openssl-3/bin/
+
+git checkout openssl_1_1_1
+if [ "$1" = "pull" ]; then
+  git pull
+fi
+
+rm -rf build
+
+OPENSSL_DIR=/opt/local ./gost-build-3.sh 2>&1 | tee ossl111-build.txt && sudo cp bin/gost.1.1.dylib /opt/local/lib/engines-1.1/ && sudo cp bin/gost*sum /opt/local/bin/ 
+
+git checkout master

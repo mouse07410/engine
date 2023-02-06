05c741348f825af7cef4eacc5a3969e807d7fcd3
Mouse <mouse008@gmail.com>
2023-02-06 10:06:46 -0500

    Updating the whole thing

--- gost-build-all.sh
+++ gost-build-all.sh
@@ -11,25 +11,30 @@ if [ "$1" = "pull" ]; then
 fi
 
 O_DIR="" 
-E_DIR="" 
+E_DIR=""
+P_DIR=""
 THRE="-3-"
 THREE="${THRE}" EOPENSSL_DIR=${O_DIR} ENGINESDIR=${E_DIR} ./gost-build-3.sh 2>&1 | tee ossl3m-build.txt
 if [ -r build/bin/gost.dylib ]; then
 	cp build/bin/gost.dylib ${HOME}/openssl-3/lib/engines-3/gost.3.0.dylib
+   	cp build/bin/gostprov.dylib ${HOME}/openssl-3/lib/ossl-modules/
 	cp build/bin/gost*sum ${HOME}/openssl-3/bin/
 fi
 
 rm -rf build
 
 O_DIR="/opt/local/libexec/openssl3" 
-E_DIR="/opt/local/libexec/openssl3/lib/engines-3" 
+E_DIR="/opt/local/libexec/openssl3/lib/engines-3"
+P_DIR="/opt/local/libexec/openssl3/lib/ossl-modules" 
 THRE="-3m-"
-THREE="${THRE}" OPENSSL_DIR=${O_DIR} ENGINESDIR=${E_DIR} ./gost-build-3.sh 2>&1 | tee ossl3-build.txt
+THREE="${THRE}" OPENSSL_DIR=${O_DIR} PROVDIR=${P_DIR} ENGINESDIR=${E_DIR} ./gost-build-3.sh 2>&1 | tee ossl3-build.txt
 if [ -r build/bin/gost.dylib ]; then
 	sudo cp build/bin/gost.dylib ${E_DIR}/gost.3.0.dylib
+   	sudo cp build/bin/gostprov.dylib ${P_DIR}/
 	sudo ln -sf ${E_DIR}/gost.3.0.dylib ${E_DIR}/gost.dylib
 	sudo ln -sf ${E_DIR}/gost.dylib /opt/local/lib/engines-3/
-	#sudo cp build/bin/gost*sum /opt/local/bin/
+   	sudo ln -sf ${P_DIR}/gostprov.dylib /opt/local/lib/ossl-modules/
+	sudo cp build/bin/gost*sum /opt/local/bin/
 fi
 
 git checkout openssl_1_1_1

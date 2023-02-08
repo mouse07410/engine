a9530f3232b90580c57a0e52374d6cbcec4e5c4f
Mouse <uri@mit.edu>
2023-02-07 21:56:07 -0500

    Fix compilation

--- gost_prov_mac.c
+++ gost_prov_mac.c
@@ -130,7 +130,7 @@ static int mac_final(void *mctx, unsigned char *out, size_t *outl,
                      size_t outsize)
 {
     GOST_CTX *gctx = mctx;
-    //unsigned int tmpoutl;
+    unsigned int tmpoutl;
     int ret = 0;
 
     /* This is strange code...  but it duplicates pkey_gost_mac_signctx() */
@@ -139,14 +139,15 @@ static int mac_final(void *mctx, unsigned char *out, size_t *outl,
         return 0;
 
     /* for platforms where sizeof(int) != * sizeof(size_t) */
-    //tmpoutl = (unsigned int)*outl;
+    tmpoutl = (unsigned int)*outl;
 
     if (out != NULL) {
         /* We ignore the error for GOST MDs that don't support setting
            the size */
         EVP_MD_CTX_ctrl(gctx->dctx, EVP_MD_CTRL_XOF_LEN, (int)gctx->mac_size, NULL);
+        ret = EVP_DigestFinal_ex(gctx->dctx, out, &tmpoutl);
         //EVP_MD_CTX_ctrl(gctx->dctx, EVP_MD_CTRL_XOF_LEN, gctx->mac_size, NULL);
-        ret = EVP_DigestFinalXOF(gctx->dctx, out, gctx->mac_size);
+        //ret = EVP_DigestFinalXOF(gctx->dctx, out, gctx->mac_size);
     }
     if (outl != NULL)
         *outl = (size_t)gctx->mac_size;
